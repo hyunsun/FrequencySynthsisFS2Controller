107f0891891008fa0fe14209aee103e5ea781ba5
Hyunsun Moon <hyunsun.moon@gmail.com>
2018-07-03 03:55:14 +0900

    Updated

--- FrequencySynthesisFS2Controller/MainForm.cs
+++ FrequencySynthesisFS2Controller/MainForm.cs
@@ -308,18 +308,17 @@ namespace FrequencySynthesisFS2Controller
                 e.Handled = true;
             }
 
-            // only allow one decimal point
+            TextBox textBox = (TextBox)sender;
             if (e.KeyChar == '.' &&
-                (sender as TextBox).Text.IndexOf('.') > -1)
+               (sender as TextBox).Text.IndexOf('.') > -1)
             {
                 e.Handled = true;
             }
 
-            if (!char.IsControl(e.KeyChar))
+            if (!char.IsControl(e.KeyChar) && textBox.SelectionLength == 0)
             {
-                TextBox textBox = (TextBox)sender;
                 if (textBox.Text.IndexOf('.') > -1 &&
-                    textBox.Text.Substring(textBox.Text.IndexOf('.')).Length >= 3)
+                    textBox.Text.Substring(textBox.Text.IndexOf('.')).Length > 1)
                 {
                     e.Handled = true;
                 }
